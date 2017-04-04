a452aa2b6c3e6ac884714de6d0122325bef1476c
Daniel Ågstrand <d.e.agstrand@gmail.com>
2017-04-04 10:43:59 +0200

    A simple image converter and folder structure

---
+++ src/main/java/bufferedimage/converter.java
@@ -0,0 +1,20 @@
+package main.java.bufferedimage;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+public class converter {
+
+
+
+public static BufferedImage convertToARGB(BufferedImage image)
+{
+    BufferedImage newImage = new BufferedImage(
+        image.getWidth(), image.getHeight(),
+        BufferedImage.TYPE_INT_ARGB);
+    Graphics2D g = newImage.createGraphics();
+    g.drawImage(image, 0, 0, null);
+    g.dispose();
+    return newImage;
+}
+}
