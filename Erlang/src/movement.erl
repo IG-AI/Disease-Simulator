305221f23e44ad0e2a46ddb9bab2f3b601e27af2
Henrik Bergendal <calidriz@gmail.com>
2017-04-26 17:44:22 +0200

    @grebsor @calidriz Removed comments

--- Erlang/src/movement.erl
+++ Erlang/src/movement.erl
@@ -43,24 +43,18 @@ generate_start_positions(Amount, {X_max,Y_max}, Result) ->
 %%
 -spec new_position(X :: integer(), Y :: integer(), Direction :: integer(), Bounds :: bounds()) -> {integer(),integer(), integer()}.
 new_position(X, Y, {X_direction, Y_direction}, {X_max, Y_max}) ->
-    %X_wall_collision_max = wall_checker:get_wall_collision(X+X_direction+2, Y),
-    %X_wall_collision_min = wall_checker:get_wall_collision(X+X_direction-2, Y),
     X_wall_collision = wall_checker:get_wall_collision(X+X_direction, Y),
     Y_wall_collision = wall_checker:get_wall_collision(X, Y + Y_direction),
-    %Y_wall_collision_max = wall_checker:get_wall_collision(X, Y + Y_direction+2),
-    %Y_wall_collision_min = wall_checker:get_wall_collision(X, Y + Y_direction-2),
 
 
     case (X+X_direction >= X_max) orelse (X+X_direction =< 0) orelse (X_wall_collision) of
 	true ->
-	    %{_, _, {New_X_direction, _}} = new_position(X,Y,people:gen_direction(),{X_max, Y_max});
 	    New_X_direction = X_direction*(-1);
 	_ ->
 	    New_X_direction = X_direction
     end,
     case (Y+Y_direction >= Y_max) orelse (Y + Y_direction =< 0) orelse (Y_wall_collision) of 
 	true ->
-	    %{_, _, {_, New_Y_direction}} = new_position(X,Y,people:gen_direction(),{X_max, Y_max});
 	    New_Y_direction = Y_direction*(-1);
 	_ ->
 	    New_Y_direction = Y_direction
