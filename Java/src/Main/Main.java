40923293a1e0fa2f26b824406143f592d57a1f00
grebsor <rosberg86@gmail.com>
2017-05-10 12:15:13 +0200

    Small changes in logical structure

--- Java/src/Main/Main.java
+++ Java/src/Main/Main.java
@@ -25,7 +25,6 @@ public class Main {
      */
     public static void main(String []args) throws InterruptedException, OtpErlangRangeException, IOException {
         javaErlangCommunicator = new JavaErlangCommunication();
-        GraphicDisplay.createAndShowGUI();
         GraphicDisplay.runSimulation();
     }
 }
